071245e8786efcf4ba0b323b244fc29b894c3792
Georgi <joroz95@gmail.com>
2023-09-19 15:11:28 +0300

    NExtjs first

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link';
+ 
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -27,10 +28,10 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Link href="/about">
+            <h2>Learn ss &rarr;</h2>            
+          </Link>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
